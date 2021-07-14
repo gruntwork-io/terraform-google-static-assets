c9ceb5159463771ca90b583b4c0f5fb62aa83e68
Ina Stoyanova <ina@gruntwork.io>
2021-07-14 17:43:54 +0300

    [IAC-1677] Replace gofmt with goimports (#19)
    * replace gofmt with goimports
    
    * run pre-commit
    
    * Revert removing test-structure
    
    * Revert removing test-structure
    
    * goimports
    
    Co-authored-by: Marina Limeira <marina@gruntwork.io>

--- test/load_balancer_website_test.go
+++ test/load_balancer_website_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/gruntwork-io/terratest/modules/test-structure"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
 func TestLoadbalancerWebsites(t *testing.T) {
